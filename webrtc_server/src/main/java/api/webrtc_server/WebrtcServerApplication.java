44c415f2dbc506641eead0bbaf2e0a45ff1f5a6d
psi7218 <psi7218@naver.com>
2025-01-05 15:20:45 +0900

    feat: backend springboot 기본 추가

---
+++ webrtc_server/src/main/java/api/webrtc_server/WebrtcServerApplication.java
@@ -0,0 +1,13 @@
+package api.webrtc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrtcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrtcServerApplication.class, args);
+	}
+
+}
