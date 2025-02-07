799b7cb6f9a2e95bc2ae7f59196ca01a481f6b4d
psi7218 <psi7218@naver.com>
2025-02-07 19:14:28 +0900

    fix: api url 수정

--- webrtc_client/src/apis/instance/axiosInstance.ts
+++ webrtc_client/src/apis/instance/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "/api/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
