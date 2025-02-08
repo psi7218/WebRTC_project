b74d664f86f002fa24c49d72e1b5200e65b9f958
psi7218 <psi7218@naver.com>
2025-02-08 20:07:30 +0900

    fix: timeout 시간 수정

--- webrtc_client/src/apis/instance/axiosInstance.ts
+++ webrtc_client/src/apis/instance/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "/api/",
-  timeout: 10000,
+  timeout: 1000,
   headers: {
     "Content-Type": "application/json",
   },
