98b4c86dc89c28f2ce978989b10c8d5166be502b
psi7218 <psi7218@naver.com>
2025-02-09 16:29:02 +0900

    fix: ci/cd 전체 과정 수정

--- webrtc_client/src/apis/instance/axiosInstance.ts
+++ webrtc_client/src/apis/instance/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "/api/",
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
